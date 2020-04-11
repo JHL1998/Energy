49a665914ef5d2194c2de5c4c5db3b0c8d0d185d
luojianhua <luojianhua@luojianhuadeMacBook-Air.local>
2020-04-11 12:53:34 +0800

    add bootstrap and  jquery

---
+++ src/main/java/com/big407/energy/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.big407.energy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){
+
+        return "index";
+    }
+}
