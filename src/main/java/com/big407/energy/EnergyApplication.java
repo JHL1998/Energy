dc7346f4662fdc5b0f03aa1277a88cdf77b1b899
罗建华 <luojianhua@luojianhuadeMacBook-Air.local>
2020-04-11 11:28:17 +0800

    init repo

---
+++ src/main/java/com/big407/energy/EnergyApplication.java
@@ -0,0 +1,13 @@
+package com.big407.energy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnergyApplication.class, args);
+    }
+
+}
